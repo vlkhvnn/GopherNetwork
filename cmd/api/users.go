1e9e48edd1d8e71f75ff85d7e73aceac263cfc21
vlkhvnn <tanir.alikh@gmail.com>
2025-01-14 17:49:05 +0500

    fix: removed unused middleware code

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"GopherNetwork/internal/store"
-	"context"
-	"errors"
 	"net/http"
 	"strconv"
 
@@ -152,30 +150,6 @@ func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Reque
 		app.internalServerError(w, r, err)
 		return
 	}
-}
-
-func (app *application) userContextMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		userId, err := strconv.ParseInt(chi.URLParam(r, "userId"), 10, 64)
-		if err != nil {
-			app.badRequestResponse(w, r, err)
-			return
-		}
-		ctx := r.Context()
-		user, err := app.store.User.GetById(ctx, userId)
-		if err != nil {
-			switch {
-			case errors.Is(err, store.ErrNotFound):
-				app.notFoundResponse(w, r, err)
-				return
-			default:
-				app.internalServerError(w, r, err)
-				return
-			}
-		}
-		ctx = context.WithValue(ctx, userCtx, user)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
 }
 
 func getUserFromContext(r *http.Request) *store.User {
