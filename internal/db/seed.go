e1ddfe0303ba3457b60c28bf078a8873a9eadc54
vlkhvnn <tanir.alikh@gmail.com>
2025-01-14 23:16:59 +0500

    fix: adding Seed function

--- internal/db/seed.go
+++ internal/db/seed.go
@@ -2,8 +2,30 @@ package db
 
 import (
 	"GopherNetwork/internal/store"
+	"context"
+	"log"
 	"math/rand"
 )
+
+func Seed(store store.Storage) {
+	ctx := context.Background()
+
+	// Assuming users are already created and fetched
+	users, err := store.User.GetAll(ctx)
+	if err != nil {
+		log.Println("Error fetching users: ", err)
+		return
+	}
+
+	// Generate and insert follower relationships
+	followers := generateFollowers(300, users)
+	for _, follower := range followers {
+		if err := store.Followers.Follow(ctx, follower.UserId, follower.FollowerId); err != nil {
+			log.Println("Error creating a follower: ", err)
+		}
+	}
+	log.Println("Seeding Followers Completed")
+}
 
 func generateFollowers(num int, users []*store.User) []*store.Follower {
 	followers := make([]*store.Follower, num)
